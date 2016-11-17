50223b7fc76f276a9f44145a984b5abbb7a1796b
CVSz <seaza@msn.com>
2016-11-17 07:12:13 +0700

    Update mssql-server-ubuntu.sh

--- mssql-server-ubuntu.sh
+++ mssql-server-ubuntu.sh
@@ -6,11 +6,12 @@
 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/msprod.list
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list > /etc/apt/sources.list.d/mssql-server.list
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install mssql-server
+sudo apt-get -y install mssql-server mssql-tools
 
 sudo /opt/mssql/bin/sqlservr-setup
 
